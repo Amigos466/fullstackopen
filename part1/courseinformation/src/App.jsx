3491bc9a16fcef325d24a41a4f3e09a6684326f2
Vladyslav Bilous <belousvladislav@gmail.com>
2024-02-23 08:04:04 +0200

    1.2: course information, step2

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -5,20 +5,20 @@ const Header = ({ course }) => (
 
 const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => (
   <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
   </>
 )
 
 const Total = ({ total }) => (
   <p>Number of exercises {total}</p>
+)
+
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
 )
 
 const App = () => {
