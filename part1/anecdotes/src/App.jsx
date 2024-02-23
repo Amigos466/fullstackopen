3eefb462f25a7f31548adf56882b58a9e60b0411
Vladyslav Bilous <belousvladislav@gmail.com>
2024-02-23 15:13:36 +0200

    1.13*: anecdotes step2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,21 +11,29 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+  const defaultVotes = anecdotes.reduce((accum, anecdote, index) => ({ ...accum, [index]: 0 }), {})
 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(defaultVotes);
 
   const selectNectRandomAnecdote = () => {
     const max = Math.floor(anecdotes.length);
     const nextAnecdoteIndex = Math.floor(Math.random() * (max - 0) + 0);
-    if(selected === nextAnecdoteIndex) {
+    if (selected === nextAnecdoteIndex) {
       return selectNectRandomAnecdote();
     }
     setSelected(nextAnecdoteIndex);
   }
 
+  const vote = () => {
+    setVotes({ ...votes, [selected]: votes[selected] + 1 })
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={vote}>vote</button>
       <button onClick={selectNectRandomAnecdote}>next anecdote</button>
     </div>
   )
